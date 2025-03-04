7c370b2065f0a474cb87990f16a0d2c9acddfcc8
Meera Bhola <meerabhola3@gmail.com>
2025-03-04 00:45:04 -0500

    changed h1 to feature my name and added container

--- my-website/src/pages/home/Home.jsx
+++ my-website/src/pages/home/Home.jsx
@@ -1,19 +1,25 @@
 import './home.css';
-import { Typography } from '@mui/material';
-//import navbar component here
+import { Container, Typography } from '@mui/material';
 
+//learn MUI
+//add grid and picture to be side by side with my name 
+//add stroke to name
+//add copy 
+
+//work on other pages 
+
+//order: projects, contact, about, resume 
 
 function Home() {
     return (
         <>
-            <div>
-                <h1>Welcome to the Home Screen</h1>
-                <Typography variant="h4" color="secondary">Hello!!!</Typography>
-                <h3 style={{ fontFamily: 'Tektur' }}>This is a demonstration of the special font</h3>
-            </div>
-            <div>
-                
-            </div>
+            <Container>
+                <div>
+                    <h1>Meera Bhola</h1>
+                    <Typography variant="h4" color="secondary">Hello!!!</Typography>
+                    <h3 style={{ fontFamily: 'Tektur' }}>This is a demonstration of the special font</h3>
+                </div>
+            </Container>
         
         
         </>
