7a1e5dc3f6c5dcc537adc42ce85e60cd1f12dd67
Meera Bhola <meerabhola3@gmail.com>
2025-03-10 18:12:44 -0400

    created icons with shadow effect

--- my-website/src/pages/home/Home.jsx
+++ my-website/src/pages/home/Home.jsx
@@ -3,6 +3,10 @@ import { Container, Typography, ThemeProvider, Box } from '@mui/material';
 import { Grid2 } from '@mui/material';
 import theme from '../../theme';
 import headshot from '../../assets/img/Headshotfx.png';
+import MouseOutlinedIcon from '@mui/icons-material/MouseOutlined';
+import KeyboardAltOutlinedIcon from '@mui/icons-material/KeyboardAltOutlined';
+import DesktopWindowsOutlinedIcon from '@mui/icons-material/DesktopWindowsOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
 
 function Home() {
     return (
@@ -33,11 +37,33 @@ function Home() {
                             </Box>
                         </Grid2>
 
-                        {/* <Grid2 item xs={12} md={3}>
-                            <Typography>Test</Typography>
-                        </Grid2> */}
-
                     </Grid2>
+
+                    <Box sx={{ mt: 12 }}>
+
+                        <Grid2 container spacing={2} justifyContent="space-between">
+
+                            <Grid2 item xs={12} md={3} display="flex" justifyContent="center"  className="mouse-icons">
+                                <MouseOutlinedIcon sx={{ color: '#AEA5D4', fontSize: '80px', filter: 'drop-shadow(4px 2px 6px rgba(185, 114, 187, 1))' }} />
+                            </Grid2>
+
+                            <Grid2 item xs={12} md={3} display="flex" justifyContent="center" className="keyboard-icons">
+                                <KeyboardAltOutlinedIcon sx={{ color: '#AEA5D4',fontSize: '80px', filter: 'drop-shadow(4px 2px 6px rgba(185, 114, 187, 1))'}} />
+                            </Grid2>
+
+                            <Grid2 item xs={12} md={3} display="flex" justifyContent="center" className="screen-icons">
+                                <DesktopWindowsOutlinedIcon sx={{ color: '#AEA5D4', fontSize: '80px', filter: 'drop-shadow(4px 2px 6px rgba(185, 114, 187, 1))'}} />
+                            </Grid2>
+
+                            <Grid2 item xs={12} md={3} display="flex" justifyContent="center" className="web-icons">
+                                <LanguageOutlinedIcon sx={{ color: '#AEA5D4', fontSize: '80px', filter: 'drop-shadow(4px 2px 6px rgba(185, 114, 187, 1))'}} />
+                            </Grid2>
+
+                        </Grid2>
+
+                    </Box>
+
+                
                 </Container>
             </ThemeProvider>
         
@@ -45,11 +71,5 @@ function Home() {
         </>
     )
 }
-
-{/* <div>
-                    <h1>Meera Bhola</h1>
-                    <Typography variant="h4" color="secondary">Hello!!!</Typography>
-                    <h3 style={{ fontFamily: 'Tektur' }}>This is a demonstration of the special font</h3>
-                </div> */}
 
 export default Home
