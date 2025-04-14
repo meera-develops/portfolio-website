f1ab12e3714b1c0f20bdc4691a4aef1b493e9bd1
Meera Bhola <meerabhola3@gmail.com>
2025-04-14 17:12:20 -0400

    added resume to project

--- my-website/src/pages/resume/Resume.jsx
+++ my-website/src/pages/resume/Resume.jsx
@@ -1,16 +1,74 @@
+import { Box, Typography, Button, Container, } from '@mui/material';
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import theme from '../../theme';
+import { ThemeProvider } from '@emotion/react';
 
-function Contact() {
+function Resume() {
     return (
         <>
-            <div>
-                <h1>View the Resume here!!</h1>
-            </div>
-            <div>
-               
-            </div>
+            <ThemeProvider theme={theme}>
+                <Container>
+                    <Box
+                        display="flex"
+                        flexDirection="column"
+                    
+                    >
+                        {/* <Box
+                            component="embed"
+                            src="/meera_bhola_resume.pdf"
+                            sx={{
+                                width: '100%',
+                                height: '600px',
+                                border: 'none',
+                                mt: 4,
+                                mb: 2,
+                            }}
+                        >
+                        </Box> */}
+                        <Box
+                            component="img"
+                            src="/meera_bhola_resume.jpg"
+                            sx={{
+                                width: '100%', 
+                                maxWidth: '600px',
+                                height: 'auto', 
+                                display: 'block', 
+                                mt: 4, 
+                                mb: 4,
+                                alignSelf: 'center',
+                                imageRendering: 'crisp-pages'
+                            }}
+                        >
+
+                        </Box>
+
+                        <Box
+                            sx={{
+                                width: 'fit-content',
+                                alignSelf: 'center',
+                                mb: 3,
+                            }}
+                        >
+                            <Button
+                                variant="contained"
+                                size="large"
+                                href="/meera_bhola_resume.pdf"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                color="accent"
+                                startIcon={<PictureAsPdfIcon />}
+                            >
+                                Download Resume
+                            </Button>
+                        </Box>
+
+                    </Box>
+
+                </Container>
+            </ThemeProvider>
         
         </>
     )
 }
 
-export default Contact
+export default Resume
